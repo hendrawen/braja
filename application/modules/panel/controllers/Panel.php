29950894259af08c7e44ef907b0286f9972a0bc4
hendrawen <hendra.faelan@gmail.com>
2018-02-27 15:59:11 +0700

    my commit

---
+++ application/modules/panel/controllers/Panel.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Panel extends CI_Controller {
+
+  function __construct()
+  {
+    parent::__construct();
+  }
+
+	public function index()
+	{
+    $data['title']          = 'Brajamarketindo';
+    $data['judul']          = 'Dashboard';
+    $data['sub_judul']      = '';
+    $data['content']        = 'content';
+		$this->load->view('dashboad', $data);
+	}
+}
